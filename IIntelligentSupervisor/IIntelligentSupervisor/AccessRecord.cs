37906d7bdecefe5deb3075c5ab565249322959c3
liujinguang <infinera@163.com>
2016-12-13 19:20:34 +0800

    check in audio, db, and email related functions

---
+++ IIntelligentSupervisor/IIntelligentSupervisor/AccessRecord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplicationTest
+{
+    public static class AccessRecord
+    {
+        public static void addRecord(String name, bool isValidate, String photoPath)
+        {
+            String datetime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            int visitorId = Visitor.getId(name);
+
+            String sqlStr = "insert into tb_user_access_records (user_id, photo_path, access_date, is_legal) values (" + visitorId + ", '" +
+                photoPath + "', '" + datetime + "', " + (isValidate ? 1 : 0) + ");";
+            Console.WriteLine(sqlStr); //2016/12/10 12:12:35
+
+            DBUtils.insert(sqlStr);
+            
+        }
+    }
+}
---
+++ IIntelligentSupervisor/IIntelligentSupervisor/EmailSender.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
+
+
+namespace ConsoleApplicationTest
+{
+    public static class EmailSender
+    {
+        public static void sendEmail() 
+        {
+            SmtpClient client = new SmtpClient("bruins.infinera.com"); //sv-smtp-prod2.infinera.com
+            //client.Host = "bruins.infinera.com";
+            //client.UseDefaultCredentials = true;
+            //client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new NetworkCredential("jliu", "SUNlight2017");
+            //client.EnableSsl = true;
+            //client.
+            MailMessage Message = new MailMessage();
+            Message.From = new MailAddress("jLiu@infinera.com");
+            Message.To.Add("jLiu@infinera.com");
+            Message.Subject = "测试标体";
+            Message.Body = "测试邮件体";
+            Message.SubjectEncoding = Encoding.UTF8;
+            Message.BodyEncoding = Encoding.UTF8;
+            Message.Priority = MailPriority.Normal;
+            Message.IsBodyHtml = true;
+            try
+            {
+                client.Send(Message);
+            }
+            catch (SmtpException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void sendEmailQQ()
+        {
+            SmtpClient smtp = new SmtpClient("smtp.qq.com");
+            // SMTP服务器用户名，密码
+            smtp.Credentials = new NetworkCredential("jinguang.liu", "zaq12wsx");
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("jinguang.liu@qq.com");   // 发件人
+            mail.To.Add("infinera@163.com");  // 收件人
+            mail.Subject = "文件已经发给你了";
+            mail.Body = "请及时查收";
+            mail.BodyEncoding = Encoding.UTF8;
+            mail.IsBodyHtml = false;
+            mail.Priority = MailPriority.Normal;
+
+            try
+            {
+                smtp.Send(mail);
+                Console.WriteLine("发送成功");
+            }
+            catch (System.Net.Mail.SmtpException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
